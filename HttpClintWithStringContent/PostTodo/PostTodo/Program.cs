50856378f0d56201c22c5d69d86fd1715c2ee5c2
Marcelo N S <marcelonespoli@outlook.com>
2021-09-18 20:51:50 +0100

    Added exmaple using srting content

---
+++ HttpClintWithStringContent/PostTodo/PostTodo/Program.cs
@@ -0,0 +1,40 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Net.Http;
+using System.Text;
+
+namespace PostTodo
+{
+    class Program
+    {
+        static async System.Threading.Tasks.Task Main(string[] args)
+        {
+            var post = new Post
+            {
+                Id = 101,
+                UserId = 1020,
+                Body = "Body test",
+                Title = "Test 1"
+            };
+
+            var client = new HttpClient();
+            //client.BaseAddress = new Uri("https://jsonplaceholder.typicode.com");
+
+
+            var httpRequestMessage = new HttpRequestMessage();
+            httpRequestMessage.Method = HttpMethod.Post;
+            httpRequestMessage.RequestUri = new Uri("https://jsonplaceholder.typicode.com/posts");
+            //foreach (var head in pHeaders)
+            //{
+            //    httpRequestMessage.Headers.Add(head.Key, head.Value);
+            //}
+
+            HttpContent httpContent = new StringContent(JsonConvert.SerializeObject(post), Encoding.UTF8, "application/json");
+            httpRequestMessage.Content = httpContent;
+
+            var result = await client.SendAsync(httpRequestMessage);
+
+            Console.WriteLine("-----------------");
+        }
+    }
+}
